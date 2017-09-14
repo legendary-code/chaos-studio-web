99354b050cef611cad90c898ea1b98a6ae543ca4
Gene Trog <gtrog@renttherunway.com>
2017-09-13 22:21:43 -0400

    Make svg responsive

--- app/src/js/pages/LogisticGraph.js
+++ app/src/js/pages/LogisticGraph.js
@@ -15,59 +15,88 @@ class LogisticGraph extends React.Component {
 
     render() {
         return (
-            <div>
-                <Paper className="logistic-settings">
-                    <div className="component-panel logistic-setting">
-                        <label className="font-setting-value-label">R</label>
-                        <Slider
-                            min={2.0}
-                            max={4.0}
-                            step={0.001}
-                            decimalPlaces={3}
-                            value={this.state.r}
-                            onValueChanged={this._rValueChanged.bind(this)}
-                            className="constant"
-                        />
-                        <div className="binding-value font-caption-medium">{this.state.r.toFixed(3)}</div>
+            <div className="logistic-graph">
+                <div className="body">
+                    <div className="contents">
+                        {this._renderGraph()}
                     </div>
-                    <div className="component-panel logistic-setting">
-                        <label className="font-setting-value-label" dangerouslySetInnerHTML={{__html: 'X<sub>0</sub>'}} />
-                        <Slider
-                            min={0.0}
-                            max={1.0}
-                            step={0.00000001}
-                            decimalPlaces={8}
-                            value={this.state.x0}
-                            onValueChanged={this._x0ValueChanged.bind(this)}
-                            className="initial-value"
-                        />
-                        <div className="binding-value font-caption-medium">{this.state.x0.toFixed(8)}</div>
-                    </div>
-                    <div className="component-panel logistic-setting">
-                        <label className="font-setting-value-label">Iterations</label>
-                        <Slider
-                            min={1}
-                            max={500}
-                            step={1}
-                            integral={true}
-                            value={this.state.iterations}
-                            onValueChanged={this._iterationsValueChanged.bind(this)}
-                            className="iterations"
-                        />
-                        <div className="binding-value font-caption-medium">{this.state.iterations}</div>
-                    </div>
-                    <div className="component-panel logistic-setting">
-                        <label className="font-setting-value-label"
-                               dangerouslySetInnerHTML={{__html: 'X<sub>0</sub> = ' + this.state.x0.toFixed(8)}} />
-                    </div>
-                    <div className="component-panel logistic-setting">
-                        <label className="font-setting-value-label"
-                               dangerouslySetInnerHTML={{__html: 'X<sub>n+1</sub> = ' + this.state.r.toFixed(3) + ' * X<sub>n</sub> * (1 - X<sub>n</sub>)'}} />
-                    </div>
-                </Paper>
-                {this._renderGraph()}
+                    <Paper className="logistic-settings">
+                        <div className="component-panel logistic-setting">
+                            <label className="font-setting-value-label"
+                                   dangerouslySetInnerHTML={{__html: 'X<sub>n+1</sub> = R * X<sub>n</sub> * (1 - X<sub>n</sub>)'}} />
+                        </div>
+                        <div className="component-panel logistic-setting">
+                            <label className="font-setting-value-label">{'Period: ' + this._calculatePeriod()}</label>
+                        </div>
+                        <div className="component-panel logistic-setting">
+                            <label className="font-setting-value-label">R</label>
+                            <Slider
+                                min={2.0}
+                                max={4.0}
+                                step={0.001}
+                                decimalPlaces={3}
+                                value={this.state.r}
+                                onValueChanged={this._rValueChanged.bind(this)}
+                                className="constant"
+                            />
+                            <div className="binding-value font-caption-medium">{this.state.r.toFixed(3)}</div>
+                        </div>
+                        <div className="component-panel logistic-setting">
+                            <label className="font-setting-value-label" dangerouslySetInnerHTML={{__html: 'X<sub>0</sub>'}} />
+                            <Slider
+                                min={0.0}
+                                max={1.0}
+                                step={0.00000001}
+                                decimalPlaces={8}
+                                value={this.state.x0}
+                                onValueChanged={this._x0ValueChanged.bind(this)}
+                                className="initial-value"
+                            />
+                            <div className="binding-value font-caption-medium">{this.state.x0.toFixed(8)}</div>
+                        </div>
+                        <div className="component-panel logistic-setting">
+                            <label className="font-setting-value-label">Iterations</label>
+                            <Slider
+                                min={1}
+                                max={500}
+                                step={1}
+                                integral={true}
+                                value={this.state.iterations}
+                                onValueChanged={this._iterationsValueChanged.bind(this)}
+                                className="iterations"
+                            />
+                            <div className="binding-value font-caption-medium">{this.state.iterations}</div>
+                        </div>
+                    </Paper>
+                </div>
             </div>
         );
+    }
+
+    _calculatePeriod() {
+        const settleIterations = 1000000;
+        const cycleIterations = 10000;
+        let x = this.state.x0;
+        let values = {};
+
+        // settle
+        for (let i = 0; i < settleIterations; ++i) {
+            x = this.state.r * x * (1-x);
+        }
+
+        // look for distinct values
+        for (let i = 0; i < cycleIterations; ++i) {
+            x = this.state.r * x * (1-x);
+            const rx = x.toFixed(10);
+            if (values.hasOwnProperty(rx)) {
+                break;
+            }
+
+            values[rx] = true;
+        }
+
+        const cycle = Object.keys(values).length;
+        return cycle === cycleIterations ? "Infinite" : cycle;
     }
 
     _renderGraph() {
@@ -75,18 +104,21 @@ class LogisticGraph extends React.Component {
         const horizontalTicks = [];
 
         for (let i = 0; i <= 100; ++i) {
-            const y = (20 + ((i / 100.0) * 60)) + '%';
+            const y = (20 + ((i / 100.0) * 70)) + '%';
             if (i % 10 === 0) {
                 verticalTicks.push(
-                    <line x1="9.5%" x2="10.5%" y1={y} y2={y} stroke="black"/>
+                    <line x1="19.5%" x2="20.5%" y1={y} y2={y} stroke="black"/>
                 );
 
                 verticalTicks.push(
-                    <text x="8.5%" y={y} alignmentBaseline="middle" textAnchor="middle">{((100 - i) / 100.0).toFixed(1)}</text>
+                    <text x="20%" y={y}
+                          transform="translate(-20, 0)"
+                          alignmentBaseline="middle"
+                          textAnchor="middle">{((100 - i) / 100.0).toFixed(1)}</text>
                 );
             } else {
                 verticalTicks.push(
-                    <line x1="9.8%" x2="10.2%" y1={y} y2={y} stroke="black"/>
+                    <line x1="19.8%" x2="20.2%" y1={y} y2={y} stroke="black"/>
                 );
             }
         }
@@ -94,12 +126,12 @@ class LogisticGraph extends React.Component {
         let value = this.state.x0;
 
         for (let i = 1; i <= this.state.iterations; ++i) {
-            const x = (10 + ((i / this.state.iterations) * 80)) + '%';
+            const x = (20 + ((i / this.state.iterations) * 60)) + '%';
             horizontalTicks.push(
-                <line x1={x} x2={x} y1="79.5%" y2="80.5%" stroke="black"/>
+                <line x1={x} x2={x} y1="89.5%" y2="90.5%" stroke="black"/>
             );
 
-            const cy = (20 + ((1.0 - value) * 60.0)) + '%';
+            const cy = (20 + ((1.0 - value) * 70.0)) + '%';
             horizontalTicks.push(
                 <circle cx={x} cy={cy} r="0.3%" fill="blue" />
             );
@@ -109,13 +141,16 @@ class LogisticGraph extends React.Component {
 
         return (
             <svg className="graph">
-                <line x1="10%" x2="10%" y1="20%" y2="80%" stroke="black" />
-                <text x="6.5%" y="50%"
+                <line x1="20%" x2="20%" y1="20%" y2="90%" stroke="black" />
+                <text x="20%" y="55%"
+                      transform="translate(-55, 0)"
                       alignmentBaseline="middle" textAnchor="middle">X</text>
                 {verticalTicks}
 
-                <line x1="10%" x2="90%" y1="80%" y2="80%" stroke="black" />
-                <text x="50%" y="83%" alignmentBaseline="middle" textAnchor="middle">Iteration</text>
+                <line x1="20%" x2="80%" y1="90%" y2="90%" stroke="black" />
+                <text x="50%" y="90%"
+                      transform="translate(0,20)"
+                      alignmentBaseline="middle" textAnchor="middle">Iteration</text>
                 {horizontalTicks}
             </svg>
         );
